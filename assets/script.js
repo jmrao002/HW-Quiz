ee97b6c108818a930f4657d15f57a1ba86cf99bf
Michael Rao <jmichaelrao@gmail.com>
2021-04-22 18:35:45 -0500

    Fix bug related to calling element by ID

--- assets/script.js
+++ assets/script.js
@@ -1,60 +1,88 @@
 // Define variables to be used to access elements via the DOM
-let timeRemaining = document.getElementById("#timeRemaining");
-let time = 15 * questions.length;
-let questionContainer = document.getElementById("#questionContainer");
-let questionText = document.getElementById("#questionText");
-let answerText = document.getElementById("#answerResult");
-let responseOptions = [
-  document.getElementById("#option0"),
-  document.getElementById("#option1"),
-  document.getElementById("#option2"),
-  document.getElementById("#option3"),
+let timeRemainingEl = document.getElementById("timeRemaining");
+let timeEl = 15 * questions.length;
+let countdownEl;
+let questionContainerEl = document.getElementById("questionContainer");
+let questionTextEl = document.getElementById("questionText");
+let responseOptionsEl = [
+  document.getElementById("option0"),
+  document.getElementById("option1"),
+  document.getElementById("option2"),
+  document.getElementById("option3"),
 ];
-let playerInitials = document.querySelector("#playerInitials");
-let alertBox = document.querySelector("#alertBox");
-let gameOver = document.querySelector("endGameContainer");
-let questionNum = 0;
-let scoresArray;
+let playerInitialsEl = document.querySelector("playerInitials");
+let alertBoxEl = document.querySelector("alertBox");
+let gameOverEl = document.querySelector("endGameContainer");
+let questionNumEl = 0;
+let scoresArrayEl;
 playerInitials.value = "";
 
 // Function that will execute upon clicking the start button
 function startQuiz() {
   // Hide title screen
   document.querySelector("#instructionsScreen").style = "display: none";
-  // start timer
-  document.querySelector("#timeRemaining").textContent = "Time: " + time;
+  // Set timer
+  document.querySelector("#timeRemaining").textContent = "Time: " + timeEl;
   // find dom element to show the question
   document.querySelector("#questionContainer").style = "display: block";
+  // Start timer
+  countdownEl = setInterval(function () {
+    timeEl--;
+    timeRemainingEl.textcontent = "Time: " + timeEl;
+    if (timeEl <= 0) {
+      clearInterval(countdownEl);
+      endGame();
+    }
+  });
   // Show first question
   getQuestion();
 }
 
 function getQuestion() {
   // Get current question
-  let questionInfo = questions[questionNum];
+  let questionInfoEl = questions[questionNumEl];
   // If there are no questions left, stop time, end function
-  if (questionInfo == undefined) {
-    clearInterval(timeLimit);
+  if (questionInfoEl == undefined) {
+    clearInterval(countdownEl);
     endGame();
   } else {
-    for (var i = 0; i < optionButtons.length; i++) {
-      optionButtons[i].textContent = i + 1 + ". " + questionInfo.choices[i];
-      optionButtons[i].value = questionInfo.choices[i];
+    // loop show choices
+    for (let i = 0; i < responseOptionsEl.length; i++) {
+      responseOptionsEl[i].textContent =
+        i + 1 + ". " + questionInfoEl.choices[i];
+      responseOptionsEl[i].value = questionInfoEl.choices[i];
     }
-    document.querySelector("#questionPrompt").textContent = questionInfo.title;
-    questionContainer.style = "display: block";
+    document.querySelector("#questionText").textContent = questionInfoEl.title;
+    // show the question
+    questionContainerEl.style.display = "block";
   }
-  // show the question
-  // loop show the choices (buttons)
-
-  // add event listener for each button created
-
   answerCheck();
 }
 
 // check user selection
 function answerCheck() {
+  let answerTextEl = document.getElementById("answerResult");
   // check the user selection against correct answer
+  if (event.target.nodeName == "button") {
+    let playerAnswerEl = event.target.value;
+    if (playerAnswerEl) {
+      if (playerAnswerEl === questions[questionNum].answer) {
+        answerResultEl = "Correct!";
+      } else {
+        answerResultEl = "Incorrect!";
+        timeEl -= 15;
+        if (timeEl <= 0) {
+          timeEl = 0;
+        }
+      }
+      // Show answerResult
+      answerTextEl.textContent = `<hr /> ${answerResult}`;
+      if (answerTextEl.style != "display: block;") {
+        answerTextEl.style = "display: block;";
+      }
+      answerTextEl.getElementById("");
+    }
+  }
   // incorrect remove seconds
   // set score
   // get next question
